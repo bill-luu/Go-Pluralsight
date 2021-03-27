c7ffcced4f879f6f46e810bf5ce5c7e6caa68c26
Bill Luu <billluu619@gmail.com>
2021-03-27 16:42:15 -0600

    Getting-Started Module 5 Demo

--- Go-Getting-Started/main.go
+++ Go-Getting-Started/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bill-luu/go-Pluralsight/Go-Getting-Started/models"
+)
 
 func main() {
-	fmt.Println("Hello from a module!")
+	u := models.User{
+		ID:        2,
+		FirstName: "Bill",
+		LastName:  "Luu",
+	}
+	fmt.Println(u)
 }
